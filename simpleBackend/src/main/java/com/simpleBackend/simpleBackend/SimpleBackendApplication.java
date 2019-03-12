e703883c6c8c78477849e4935fbc940cf4a335f5
Marc Bengsch <mbengsch@me.com>
2019-03-12 09:31:28 +0100

    baseProject

---
+++ simpleBackend/src/main/java/com/simpleBackend/simpleBackend/SimpleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simpleBackend.simpleBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBackendApplication.class, args);
+	}
+
+}
