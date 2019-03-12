83a6ecf1892a8389b1b334797d3daafa2d2bd740
Marc Bengsch <mbengsch@me.com>
2019-03-12 11:36:58 +0100

    added

--- simpleBackend/src/main/java/com/simpleBackend/simpleBackend/web/PostWebService.java
+++ simpleBackend/src/main/java/com/simpleBackend/simpleBackend/web/PostWebService.java
@@ -4,12 +4,14 @@ package com.simpleBackend.simpleBackend.web;
 import com.simpleBackend.simpleBackend.data.PostRepository;
 import com.simpleBackend.simpleBackend.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin
 @RestController
 public class PostWebService {
 
@@ -19,5 +21,12 @@ public class PostWebService {
 	@GetMapping("/")
 	public @ResponseBody List<Post> getAllPosts(){
 		return (List<Post>) postRepository.findAll();
+	}
+
+	@RequestMapping(value = "/addPost",method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<String> addPost(@RequestBody Post post){
+		System.out.println(post);
+		postRepository.save(post);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 }
